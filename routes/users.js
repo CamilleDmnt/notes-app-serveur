19b50e853162f2d8130e66524794272a4ea4b70d
GRETA-CPIG\camille.dumontier <camille.dumontier@greta-cfa.academy>
2024-03-05 13:36:55 +0100

    add files and database users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
-var router = Router();
+import { dbQuery } from '../db';
+const router = Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', async function (req, res) {
+  // const username = req.body.username;
+  // const password = req.body.password;
+
+  const {username, password} = req.body;
+
+  const [result] = await dbQuery.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password]);
+  res.json({message: 'user created', result: result });
 });
 
 export default router;
