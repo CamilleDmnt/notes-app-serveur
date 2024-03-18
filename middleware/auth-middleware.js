1d9496895b0f91dcfb597834e6622659f6a1bac2
GRETA-CPIG\camille.dumontier <camille.dumontier@greta-cfa.academy>
2024-03-18 16:56:25 +0100

    connexion bdd

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,16 +1,17 @@
 import jwt from 'jsonwebtoken';
 
 // Middleware : intercepte la req et vérifie le token fourni
-// si tout se passe bien o passe la main au contrôleur grace à next()
+// si tout se passe bien on passe la main au contrôleur grace à next()
 // si problème on répond avec une erreur et le contrôleur ne sera pas déclenché.
 export function verifyToken(req, res, next) {
   // le token devrait être présenté dans le header
   const token = req.header('Authorization');
+  console.log('check jwt, headers are: ', req.headers);
   // 401 est le code pour dire "interdit"
   if (!token) return res.status(401).json({ error: 'Access denied' });
   try {
     // on vérifie le token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     // on enrichie la requête en y plaçant l'id du user
     // on décode le token qui nous donne le user.id
     // on pourra donc aller piocher dans req.userId depuis les contrôleurs.
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 import { Router } from 'express';
-var router = Router();
+import { dbQuery } from '../db.js';
+
+const router = Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', async function (req, res) {
+  // const username = req.body.username;
+  // const password = req.body.password;
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const [result] = await dbQuery('INSERT INTO users (username, password) VALUES (?, ?)', [username, hashedPassword]);
+  res.json({ message: 'user created', result: result });
+});
+router.post('/login', async function (req, res) {
+  const { username, password } = req.body;
+  // rechercher le user 
+  const [result] = await dbQuery("SELECT * FROM users WHERE username = ?", [username]);
+  const user = result[0];
+  if (!user) {
+    return res.status(401).json({ error: 'Authentification failed' })
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    return res.status(401).json({ error: 'Authentification failed' });
+  }
+  // clé privée
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+  res.json({ message: 'ok', token: token });
 });
 
 export default router;
